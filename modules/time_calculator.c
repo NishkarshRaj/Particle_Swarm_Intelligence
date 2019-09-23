a39830e65b483f3d080059466e1e0a5a9c7baae4
lakshika <lakshika@localhost.localdomain>
2019-09-23 10:35:47 +0530

    added time_calculator module

---
+++ modules/time_calculator.c
@@ -0,0 +1,19 @@
+// #include <time.h> header needed to use the function 
+
+
+time_calculate()
+{
+//clock_t is capable of calculating processor time and clock funcion is capable of returning the clock time since the program is started
+clock_t start = clock(); 
+
+/*
+write the program for which you need to calculate the time used by cpu
+*/
+
+clock_t end = clock();
+double time = ((double) (end - start)) / CLOCKS_PER_SEC;    //number of processor clock ticks per second is CLOCKS_PER_SEC
+}
+
+
+
+
