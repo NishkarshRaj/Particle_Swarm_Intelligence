b7a84958b5f6d94d65b75da6111f1a7d3dedc33a
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-10-06 16:45:25 +0530

    Module 5 complete

--- modules/Module_5_AdjancyMTL.c
+++ modules/Module_5_AdjancyMTL.c
@@ -1,107 +1,131 @@
-//libraries needed
 #include<stdio.h>
-#include<stdlib.h>  //for malloc and system function
-
-
-struct n	
+#include<stdlib.h>
+//Adjancy List
+//N nodes -> 0 is V1, 1 is V2,,, k is Vk+1,,,, n-1 is Vn
+//Adjancy List ignoring loop, if connected with rest, then added in ordered fashion
+struct node
 {
-int data;
-struct n *link;
+	int node_number;
+	struct node *next; //for linked list of structures
+	struct node *list[20]; //for Adjancy list
 };
-
-
-typedef struct n NODE;
-
-NODE *getnode(int j)			//this function is used when there is a connection between the nodes
-{
-NODE * temp;
-temp=(NODE *)malloc(sizeof(NODE));	//allocating memory to the temp dynamically	
-temp->data=j;
-temp->link=NULL;
-return(temp);
-}
-
-
-NODE *findlast(NODE *h)		//This function is called when there is no connection between the nodes
-{
-NODE *ptr;
-for(ptr=h;ptr->link!=NULL;ptr=ptr->link);
-return(ptr);
-}
-
-
-void display(NODE *h[10],int n)		//this function is displaying the list
-{
-NODE *ptr;
-
-for(int i=0;i<n;i++)
-	{
-	printf("\n V%d",i+1);		
-	ptr=h[i];
-	if(ptr==NULL)
-		printf(" NULL");
-	while(ptr!=NULL)
-	{
-	printf(" ->V%d",ptr->data);
-	ptr=ptr->link;
-	}
-	printf("\n");
-	}
-}
-
+// Create a linked list
 int main()
 {
-NODE *ptr,*temp,*h[10];
-int n,a[10][10],i,j;
-
-printf("\n Enter total number of vertices : ");
-scanf("%d",&n);
-
-printf("\n Enter entries of an adjacency matrix :\n");
-for(i=0;i<n;i++)
-	for(j=0;j<n;j++)
+	node *ptr,*head,*p;
+	int n,i,j,k;
+	scanf("%d",&n); //Number of vertex in the network
+	int matrix[n][n];
+	node *list1[n];
+	for(i=0;i<n;i++)
 	{
-	printf("\n Enter a[%d][%d] : ",i+1,j+1);  
-	scanf("%d",&a[i][j]);
-	}
-
-system("clear");			//after entering the adjancency matrix we are clearing the screen using the function system
-
-
-//printing the adjancency matrix 
-printf("\n\nAdjacency matrix is  \n");		
-for(i=0;i<n;i++)
-{
-for(j=0;j<n;j++)
-{
-printf(" %d ",a[i][j]);
-}
-printf("\n");
-}
-
-for(i=0;i<n;i++)
-h[i]=NULL;			//making the values of pointer array as NULL
-
-
-for(i=0;i<n;i++)			//from here we are converting the Adjacency matrix into Adjacency list
-	for(j=0;j<n;j++)
-	{
-		if(a[i][j]==1)
+		for(j=0;j<n;j++)
 		{
-		temp=getnode(j+1);
-		if(h[i]==NULL)
-		h[i]=temp;
+			scanf("%d",&matrix[i][j]);
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("%d\t",matrix[i][j]);
+		}
+		printf("\n");
+	}
+	////////////////////////////////////////////////
+k=1;
+	//Create Linked list for all the Nodes in Network
+	for(i=0;i<n;i++)
+	{
+		ptr = (node*)malloc(sizeof(node));
+		//printf("Enter the Unique number for Node: ");
+		//scanf("%d",&(ptr->node_number));
+ptr->node_number=k;
+k++;
+		//ptr->list=NULL;
+		ptr->next=NULL;
+		list1[i]=(node*)malloc(sizeof(node));
+		for(j=0;j<20;j++)
+		{
+			list1[i]->list[j]=NULL;
+		}
+		if(i==0)
+		{
+			head=(node*)malloc(sizeof(node));
+			head=ptr;	
+		}
 		else
 		{
-		ptr=findlast(h[i]);
-		ptr->link=temp;
+			p=head;
+			while(p->next!=NULL)
+			{
+				p=p->next;
+			}
+			p->next=ptr;
 		}
-		}
+		list1[i]=ptr;
 	}
-
-printf("\n The Adjacency list \n");	//size of the list will be equal to the no. of vertices
-display(h,n);
-return 0;
+	////////////////////////////////////////////////
+	//Display the Linked List
+	p=head;
+	printf("%d\n",head->node_number);
+	while(p->next!=NULL)
+	{
+		printf("%d\n",ptr->node_number);
+		p=p->next;
+	}
+	//printf("%d",list1[0]->node_number);
+	/////////////////////////////////////////////////
+	//Create List
+	p=head;
+	while(p->next!=NULL)
+	{
+//printf("Check 1\t");
+		j=0;
+k=p->node_number;
+k--;
+		for(i=0;i<n;i++)
+		{
+//	printf("Check 2\t");
+			if((matrix[k][i]!=0)&&(i!=k))
+			{
+//printf("Check 3\n");
+				p->list[j++]=list1[i];	
+			}
+		}
+p=p->next;
+	}
+//printf("Check4");
+		j=0;
+k=p->node_number;
+k--;
+		for(i=0;i<n;i++)
+		{
+//	printf("Check 2\t");
+			if((matrix[k][i]!=0)&&(i!=k))
+			{
+//printf("Check 3\n");
+				p->list[j++]=list1[i];	
+			}
+		}
+	/////////////////////////////////////////////////
+	//Display Adjancy List
+	p=head;
+	while(p->next!=NULL)
+	{
+printf("%d: ",p->node_number);
+		for(i=0;p->list[i]!=NULL;i++)
+		{
+			printf("%d\t",p->list[i]->node_number);
+		}
+printf("\n");
+p=p->next;
+	}
+printf("%d: ",p->node_number);
+		for(i=0;p->list[i]!=NULL;i++)
+		{
+			printf("%d\t",p->list[i]->node_number);
+		}
+printf("\n");
+	return 0;
 }
-
-
