5946d4c71ddc070a2634294255c0bbeb896fd46e
NishkarshRaj <500060720@stu.upes.ac.in>
2019-10-06 15:22:13 +0530

    Adjancy Matrix part done| Structures done

--- modules/Adjancy.c
+++ modules/Adjancy.c
@@ -3,7 +3,7 @@
 //Adjancy List
 //N nodes -> 0 is V1, 1 is V2,,, k is Vk+1,,,, n-1 is Vn
 //Adjancy List ignoring loop, if connected with rest, then added in ordered fashion
-typedef struct node
+struct node
 {
 	int node_number;
 	struct node *next; //for linked list of structures
@@ -12,11 +12,6 @@ typedef struct node
 // Create a linked list
 int main()
 {
-	node v1 = (node*)malloc(sizeof(node));
-	v1->node_number=1;
-	v1->next=NULL;
-	vi->list=NULL;
-	printf("%d",v1->node_number);
 	int n,i,j;
 	scanf("%d",&n); //Number of vertex in the network
 	int matrix[n][n];
