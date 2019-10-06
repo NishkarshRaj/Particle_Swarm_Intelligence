20af04636aaa64682f9f80a03b071b80b7a2d14d
NishkarshRaj <500060720@stu.upes.ac.in>
2019-10-06 12:55:13 +0530

    Module_3_TimeCalculator.c

--- modules/Module_3_TimeCalculator.c
+++ modules/Module_3_TimeCalculator.c
@@ -1,7 +1,7 @@
-// #include <time.h> header needed to use the function 
+#include <time.h> header needed to use the function 
+#include<stdio.h>
 
-
-time_calculate()
+int main()
 {
 //clock_t is capable of calculating processor time and clock funcion is capable of returning the clock time since the program is started
 clock_t start = clock(); 
@@ -12,6 +12,7 @@ write the program for which you need to calculate the time used by cpu
 
 clock_t end = clock();
 double time = ((double) (end - start)) / CLOCKS_PER_SEC;    //number of processor clock ticks per second is CLOCKS_PER_SEC
+printf("%d\n",time);
 }
 
 
