f58cf7fe9ccd19dac0755c5b0d4cb8c27765b080
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-09 17:26:21 +0530

    Midsem integrated till module 4

--- modules/Midsem.c
+++ modules/Midsem.c
@@ -2,6 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<dirent.h>
+#define NODES 50
 //User creates graph as .csv file
 void create_csv()
 {
@@ -72,6 +73,70 @@ write the program for which you need to calculate the time used by cpu
 clock_t end = clock();
 clock_t time = ((double) (end - start)) / CLOCKS_PER_SEC;    //number of processor clock ticks per second is CLOCKS_PER_SEC
 printf("%d\n",time);
+}
+
+//Module 4
+void readcsv(char f1[100])
+{
+
+  // Declare the file pointer 
+  FILE * filePointer;
+
+  // Declare the variable for the data to be read from file 
+  char dataToBeRead[50];
+
+  // Open the existing file GfgTest.c using fopen() 
+  // in read mode using "r" attribute 
+  filePointer = fopen(f1, "r");
+
+  // Check if this filePointer is null 
+  // which maybe if the file does not exist 
+  if (filePointer == NULL) {
+    printf("test.csv file failed to open.");
+  } else {
+
+    printf("The file is now opened.\n");
+    int count = 0;
+    int k = 10;
+    char arr[NODES][NODES];
+    while (fgets(dataToBeRead, 50, filePointer) != NULL) {
+      if (count == 0) {
+        // First line of csv contains total number of nodes
+        k = atoi(dataToBeRead);
+      } else {
+        int j = 0;
+        int length = (int) strlen(dataToBeRead);
+        for (int i = 0; i < length - 1; i++) {
+
+          if (dataToBeRead[i] != ',') {
+
+            arr[count - 1][j] = dataToBeRead[i];
+
+            // make sure to print using %c 
+            printf("%c \n", arr[count - 1][j]);
+            j++;
+          }
+        }
+        printf("\n");
+
+      }
+      count++;
+    }
+
+    // Closing the file using fclose() 
+    fclose(filePointer);
+
+    printf("Data successfully read from specified file\n");
+    printf("The file is now closed.\n");
+    int array[k][k];
+    for (int i = 0; i < k; i++) {
+      for (int j = 0; j < k; j++) {
+        array[i][j] = arr[i][j] - '0';
+        printf("%d ", array[i][j]);
+      }
+      printf("\n");
+    }
+  }
 }
 
 int main()
