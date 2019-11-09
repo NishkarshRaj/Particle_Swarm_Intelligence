97afe4f50d4ec70af955664e89c4b41c981364e5
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-09 17:18:40 +0530

    Integrated till Module 3

--- modules/Midsem.c
+++ modules/Midsem.c
@@ -60,6 +60,20 @@ static int parse_ext(const struct dirent *dir)
      }
      return 0;
 }
+
+//Time calculator
+void time()
+{
+//clock_t is capable of calculating processor time and clock funcion is capable of returning the clock time since the program is started
+clock_t start = clock(); 
+/*
+write the program for which you need to calculate the time used by cpu
+*/
+clock_t end = clock();
+clock_t time = ((double) (end - start)) / CLOCKS_PER_SEC;    //number of processor clock ticks per second is CLOCKS_PER_SEC
+printf("%d\n",time);
+}
+
 int main()
 {
 int ch1,n,count;
