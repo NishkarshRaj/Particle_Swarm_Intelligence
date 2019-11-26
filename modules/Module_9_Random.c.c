5dafdf986151051848cc96159a064bec78e34179
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-11-26 15:10:50 +0530

    Module 9 Random Number added

---
+++ modules/Module_9_Random.c.c
@@ -0,0 +1,35 @@
+// C program to generate random numbers 
+#include<math.h>
+#include<stdio.h>  //Io Header File
+#include<stdlib.h> //For Rand and Srand inbuilt functions 
+#include<time.h> //To generate Random numbers each and every time rather than getting same random number 
+//Time function is needed to generate dynamic numbers at runtime because rand() is compiled time executed and generates same number on multiple execution
+  
+int random_generator()
+{
+	return (rand());
+}  
+  
+// Driver program 
+int main(void) 
+{ 
+	//Initialization used to call only once
+	srand(time(0)); 
+	register int x = random_generator(); //Using register storage class for faster access
+	//printf("%d",x); Yes every time new random integer is generated
+	//How to convert any integer number into a float between 0 and 1
+	//Divide the number by 10^(number of digits)
+	int count=0,temp;
+	temp=x; //temp holds x value because its going to change to 0 to count frequency of digits
+	while(temp>0)
+	{
+	    count++;
+	    temp=temp/10;
+	}
+	printf("%d\n%d\n",x,count);
+	temp = pow(10,count); //using same variable again to save memory
+	float answer = (float)x/temp;
+	printf("%f\n",answer);
+    return 0; 
+} 
+
