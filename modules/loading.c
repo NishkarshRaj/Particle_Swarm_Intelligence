485434eb1ef6d8e687ff56128e38075feff2d0ed
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-12-08 16:39:09 +0530

    Loading function

--- modules/loading.c
+++ modules/loading.c
@@ -1,16 +1,20 @@
 #include<stdio.h>
 #include<cstdlib>
 using namespace std;
-int main()
+void loading()
 {
 int i;
 char ch;
 system("reset");
 printf("Hello, Enter any key to continue: ");
 scanf("%c",&ch);
-for(i=0;i<10000;i++)
+for(i=0;i<50000;i++)
 {
 printf("Loading Content: %d\n",i+1);
 }
 system("reset");
+}
+int main()
+{
+loading();
 }
