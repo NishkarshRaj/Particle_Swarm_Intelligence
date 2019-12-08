bbb32db9bf0dc2caaaf344c807bcd0eef77b2356
Nishkarsh Raj <500060720@stu.upes.ac.in>
2019-12-08 19:17:53 +0530

    Main updated with prims

--- modules/Main.c
+++ modules/Main.c
@@ -24,6 +24,58 @@ printf("Loading Content: %d\n",i+1);
 system("reset");
 }
 
+//Prims Helper Function
+int isValidEdge(int u, int v, int* inMST) 
+{ 
+   if (u == v) 
+       return 0; 
+   if (inMST[u] == 0 && inMST[v] == 0) 
+        return 0; 
+   else if (inMST[u] == 1 && inMST[v] == 1) 
+        return 0; 
+   return 1; 
+} 
+  
+void primMST(int cost[][50],int x) 
+{   
+clock_t start = clock();
+    int inMST[n]; 
+  
+    // Include first vertex in MST 
+    inMST[0] = 1; 
+  
+    // Keep adding edges while number of included 
+    // edges does not become V-1. 
+    int edge_count = 0, mincost = 0; 
+    while (edge_count < x - 1) { 
+  
+        // Find minimum weight valid edge.   
+        int min = INT_MAX, a = -1, b = -1; 
+        for (int i = 0; i < x; i++) { 
+            for (int j = 0; j < x; j++) {                
+                if (cost[i][j] < min) { 
+                    if (isValidEdge(i, j, inMST)) { 
+                        min = cost[i][j]; 
+                        a = i; 
+                        b = j; 
+                    } 
+                } 
+            } 
+        } 
+        if (a != -1 && b != -1 ) { 
+            printf("Edge %d:(%d, %d) cost: %d \n",  
+                         edge_count++, a, b, min); 
+            mincost = mincost + min; 
+            inMST[b] = inMST[a] = 1; 
+        } 
+    } 
+    clock_t end = clock();
+    printf("\nMinimum cost= %d \n", mincost); 
+    double time = ((double) (end - start))/CLOCKS_PER_SEC;
+    time=time*1000000;
+    printf("Processing Time: %f MicroSeconds\n",time);
+} 
+
 int parent[INT_MAX];
 
 //Kruskals Helper Function
@@ -277,6 +329,8 @@ system("reset");
 		  kruskalMST(matrix,n); 
   			break;
   		case 2:
+  			printf("Prim's Algorithm Application\n");
+  			primMST(matrix,n);
   			break;
   		case 3:
   			break;
