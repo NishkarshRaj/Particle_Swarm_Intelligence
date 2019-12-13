508ba0057178e14d59f3fcdc5a823c9d12a03bf2
NishkarshRaj <500060720@stu.upes.ac.in>
2019-12-13 10:31:03 +0530

    Testing fails :(

--- modules/Test.cpp
+++ modules/Test.cpp
@@ -8,16 +8,16 @@ int main() {
   FILE * filePointer;
 
   // Declare the variable for the data to be read from file 
-  int dataToBeRead[50];
+  char dataToBeRead[50];
 
   // Open the existing file GfgTest.c using fopen() 
   // in read mode using "r" attribute 
-  filePointer = fopen("First.csv", "r");
+  filePointer = fopen("e.csv", "r");
 
   // Check if this filePointer is null 
   // which maybe if the file does not exist 
   if (filePointer == NULL) {
-    printf("test.csv file failed to open.");
+    printf("File failed to open");
   } else {
 
     printf("The file is now opened.\n");
@@ -32,25 +32,42 @@ int main() {
         int j = 0;
         int length = (int) strlen(dataToBeRead);
         for (int i = 0; i < length - 1; i++) {
-          if (dataToBeRead[i] != ',') {
-            arr[count - 1][j] = dataToBeRead[i];
+
+/*          if (dataToBeRead[i] != ',') {
+
+            arr[count - 1][j] = (int) dataToBeRead[i];
+
             // make sure to print using %c 
-            printf("%c \n", arr[count - 1][j]);
+            printf("%d \n", arr[count - 1][j]);
             j++;
           }
+*/
+      if (dataToBeRead[i] != ',') {
+
+            arr[count - 1][j] = (int) dataToBeRead[i];
+
+            // make sure to print using %c 
+            printf("%d \n", arr[count - 1][j]);
+            j++;
+          }
+
         }
         printf("\n");
+
       }
       count++;
     }
+
     // Closing the file using fclose() 
     fclose(filePointer);
+
     printf("Data successfully read from specified file\n");
     printf("The file is now closed.\n");
     int array[k][k];
     for (int i = 0; i < k; i++) {
       for (int j = 0; j < k; j++) {
-        array[i][j] = arr[i][j] - '0';
+       	array[i][j] = arr[i][j] - '0';
+
         printf("%d ", array[i][j]);
       }
       printf("\n");
